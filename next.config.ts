287f9780c7115cda00799e85c69144c086d979a0
TeguhKrisnha-NK <teguhnk0@gmail.com>
2025-11-30 09:55:57 +0800

    Fix: Remove deprecated swcMinify option

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   // reactCompiler: true,  // COMMENT TEMPORARILY
-  swcMinify: true,
   compress: true,
 
   experimental: {
